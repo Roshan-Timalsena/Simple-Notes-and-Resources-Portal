b0c6e4b50a8c612498015b10677d126a9875de35
Roshan-Timalsena <62935236+Roshan-Timalsena@users.noreply.github.com>
2021-07-19 10:48:39 +0545

    linking in production

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Note;
 use App\Http\Controllers\NoteController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,5 +24,11 @@ Route::get('/dashboard', [NoteController::class, 'index'])->middleware(['auth'])
 Route::post('/dashboard/upload-notes', [NoteController::class, 'saveNotes'])->middleware(['auth'])->name('note.upload');
 Route::get('/notes', [NoteController::class, 'getNotes'])->middleware(['auth'])->name('notes.all');
 Route::get('/notes/delete/{note}', [NoteController::class, 'removeNote'])->middleware(['auth'])->name('notes.remove');
+
+//Route for linking storage to public folder in production stage
+// Route::get('/storage/docs/{note:document}', function(Note $note){
+//     Artisan::call('storage:link');
+//     return redirect("/storage/docs/$note");
+// });
 
 require __DIR__.'/auth.php';
